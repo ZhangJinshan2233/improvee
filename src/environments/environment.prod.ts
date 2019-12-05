f3f344d516dc655f2194120e875876f5e434a4ad
Jinshan <zhangjs2233@gmail.com>
2019-12-05 15:32:04 +0800

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'http://34.87.94.22',
+  ONESIGNAL_ID:"83cea0f8-1467-4ecd-a670-ccd8d94c56c8",
+  ANDROID_ID:"753824683630"
 };
